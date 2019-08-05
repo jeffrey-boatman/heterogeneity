627ba03feb4ca35aa34c40647e12b7f4aa9657ab
jeffrey-boatman <boat0036@umn.edu>
2019-08-05 17:09:58 -0500

    added additional R files

---
+++ permutation-simulation/permutation-simulation.R
@@ -0,0 +1,107 @@
+library(parallel)
+
+
+sim <- function(seed, coefs, n_perm = 1000) {
+  set.seed(seed)
+  n  <- 1e2
+  x1 <- rnorm(n)
+  x2 <- x1 + rnorm(n)
+  a  <- rbinom(n, 1, 1 / 2)
+
+  fm <- ~ a * (x1 + x2)
+  dm <- model.matrix(fm)
+  y <- rnorm(n, mean = c(dm %*% coefs))
+
+  chuyu_pe <- chuyu_est(y, a, x1, x2, FALSE)
+  chuyu_pd <- replicate(n_perm, chuyu_est(y, a, x1, x2, TRUE))
+
+    
+  jeff_pe <- jeff_est(y, a, x1, x2, FALSE)
+  jeff_pd <- replicate(n_perm, jeff_est(y, a, x1, x2, TRUE))
+
+  david_pe <- david_est(y, a, x1, x2, FALSE)
+  david_pd <- replicate(n_perm, david_est(y, a, x1, x2, TRUE))
+
+  out <- list()
+  out$chuyu_pval  <- mean(chuyu_pe > chuyu_pd)
+  out$jeff_pval   <- mean(jeff_pe > jeff_pd)
+  out$david_pval  <- mean(david_pe > david_pd)
+  unlist(out)
+}
+
+jeff_est <- function(y, a, x1, x2, permute = FALSE) {
+  n <- length(y)
+  index <- seq_len(n)
+  if (permute)
+    index <- sample(index)
+  data <- data.frame(y = y, a = a, x1 = x1[index], x2 = x2[index])
+  d0 <- data[data$a == 0, ]
+  d1 <- data[data$a == 1, ]
+  m0 <- lm(y ~ x1 + x2, data = d0)
+  m1 <- lm(y ~ x1 + x2, data = d1)
+  p0 <- predict(m0, newdata = data)
+  p1 <- predict(m1, newdata = data)
+  z  <- p1 - p0
+  sd(z)
+}
+
+chuyu_est <- function(y, a, x1, x2, permute = FALSE) {
+  n <- length(y)
+  index <- seq_len(n)
+  if (permute)
+    index <- sample(index)
+  data <- data.frame(y = y, a = a[index], x1 = x1, x2 = x2)
+  d0 <- data[data$a == 0, ]
+  d1 <- data[data$a == 1, ]
+  m0 <- lm(y ~ x1 + x2, data = d0)
+  m1 <- lm(y ~ x1 + x2, data = d1)
+  p0 <- predict(m0, newdata = data)
+  p1 <- predict(m1, newdata = data)
+  z  <- p1 - p0
+  sd(z)
+}
+
+david_est <- function(y, a, x1, x2, permute = FALSE) {
+  n <- length(y)
+  index <- seq_len(n)
+  data <- data.frame(y = y, a = a, x1 = x1, x2 = x2)  
+
+  if (permute) {
+    index <- sample(index)    
+    data$y[data$a == 1] <-  data$y[data$a == 1] - 
+      (mean(data$y[data$a == 1]) - mean(data$y[data$a == 0]))
+    #data <- data.frame(y = y, a = a[index], x1 = x1, x2 = x2)
+    data$a <- data$a[index]
+  }
+
+  d0 <- data[data$a == 0, ]
+  d1 <- data[data$a == 1, ]
+  m0 <- lm(y ~ x1 + x2, data = d0)
+  m1 <- lm(y ~ x1 + x2, data = d1)
+  p0 <- predict(m0, newdata = data)
+  p1 <- predict(m1, newdata = data)
+  z  <- p1 - p0
+  sd(z)
+}
+
+# type I error ----
+coefs <- c(1, 1, 1, 1, 0, 0)
+
+out <- mclapply(seq_len(1000),
+  FUN      = sim,
+  coefs    = coefs,
+  n_perm   = 1000,
+  mc.cores = 24)
+out <- do.call(rbind, out)
+apply(out, 2, function(x) mean(x < 0.05))
+
+# power ----
+coefs <- c(1, 1, 1, 1, 1, 1)
+
+out <- mclapply(seq_len(1000),
+  FUN      = sim,
+  coefs    = coefs,
+  n_perm   = 1000,
+  mc.cores = 24)
+out <- do.call(rbind, out)
+apply(out, 2, function(x) mean(x < 0.05))
